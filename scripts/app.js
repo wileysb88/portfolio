22f24e28fb13de3845eaa4f8ccc613b63083f601
Scott Wiley <wileysb@gmail.com>
2016-05-02 19:26:18 -0500

    added carousel and contact icons

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,12 @@
 $( document ).ready(function(){
    $(".button-collapse").sideNav();
    $('.parallax').parallax();
+   $('.slider').slider({full_width: true});
 })
+
+var options = [
+   {selector: '.first', offset: 200, callback: function() {
+        Materialize.fadeInImage(".first");
+      } },
+ ];
+ Materialize.scrollFire(options);

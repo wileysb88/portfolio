7fd6062217a4c5fa14d039b146b8ddb4ebe1b6af
Scott Wiley <wileysb@gmail.com>
2016-05-04 09:34:44 -0500

    lts of background changes and finalizing style

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,9 @@
 $( document ).ready(function(){
    $(".button-collapse").sideNav();
    $('.parallax').parallax();
-   $('.slider').slider({full_width: true});
-})
+   $('.slider').slider({full_width: true});]
+    $('#fullpage').fullpage();
+});
 
 var options = [
    {selector: '.first', offset: 200, callback: function() {

9928cd17c82c3d315efeb312a058cb24019481ef
Scott Wiley <wileysb@gmail.com>
2016-05-05 12:17:19 -0500

    got scroll to work

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,18 @@
 $( document ).ready(function(){
    $(".button-collapse").sideNav();
    $('.slider').slider({full_width: true});
+
+
+  $('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top - 60
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+
 });
-
-
-var options = [
-   {selector: '.first', offset: 200, callback: function() {
-        Materialize.fadeInImage(".first");
-      } },
- ];
- Materialize.scrollFire(options);
